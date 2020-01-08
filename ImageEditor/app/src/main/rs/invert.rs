606ddb954ad2e9642ace1c5d2bdb9cd0b32e3c5b
Elliot Renel <elliot.renel@etu.u-bordeaux.fr>
2020-01-08 23:42:39 +0100

    Suppression de l'ancien dossier pour les tests

--- ImageEditor/app/src/main/rs/invert.rs
+++
@@ -1,8 +0,0 @@
-#pragma  version (1)
-#pragma  rs  java_package_name(com.example.imageeditor.rssample)
-
-uchar4  RS_KERNEL  invert(uchar4 in, uint32_t x, uint32_t y) {
-    uchar4  out = in;out.r = 255 - in.r;
-    out.g = 255 - in.g;out.b = 255 - in.b;
-    return  out;
-}
